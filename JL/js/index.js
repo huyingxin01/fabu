7f4705f5402230ae4b9bd5a86b269f8afef25ff7
huyingxin01 <512272399@qq.com>
2016-07-26 22:42:31 +0800

    00

---
+++ JL/js/index.js
@@ -0,0 +1,44 @@
+new Swiper(".swiper-container", {
+    direction: "vertical",
+    loop: true,
+    onSlideChangeEnd: function (swipe) {
+        var n = swipe.activeIndex,
+            slideAry = swipe.slides;
+        [].forEach.call(slideAry, function (item, index) {
+            if (index == n) {
+              /*  item.id = index == 1 || index == 3 ? "page1" : "page2";*/
+                if(index==1){
+                    item.id="page1";
+                }else if(index==2){
+                    item.id="page2";
+                }else if(index==3){
+                    item.id="page3";
+                }else if(index==4){
+                    item.id="page4";
+                }else if(index==5){
+                    item.id="page1";
+                }
+                return;
+            }
+            item.id = null;
+        });
+    }
+});
+
+var music = document.getElementById("music"),
+    musicAudio = document.getElementById("musicAudio");
+window.setTimeout(function () {
+    musicAudio.play();
+    musicAudio.addEventListener("canplay", function () {
+        music.style.display = "block";
+    }, false);
+}, 1000);
+music.addEventListener("click", function () {
+    if (musicAudio.paused) {
+        musicAudio.play();
+        music.className = "music musicMove";
+        return;
+    }
+    musicAudio.pause();
+    music.className = "music";
+}, false);
